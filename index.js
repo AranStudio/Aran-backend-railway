dba685fa3ae62ee031a0aceae36e29892c269bce
AranStudio <Rileyscottneal@gmail.com>
2025-12-15 21:56:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import router from "./routes/router.js"; // <-- adjust if needed
+import router from "./router.js"; // SAME FOLDER. THIS IS IMPORTANT.
 
 const app = express();
 
