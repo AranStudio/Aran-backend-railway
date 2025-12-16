bff994faf935f3e8e50ea93c63242536c9aee4ae
AranStudio <Rileyscottneal@gmail.com>
2025-12-15 22:07:38 -0800

    Create generatestoryboards.js

---
+++ routes/generatestoryboards.js
@@ -0,0 +1,29 @@
+
+import express from "express";
+import { openai } from "../utils/openaiClient.js";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const { frames } = req.body;
+
+    const images = [];
+
+    for (const frame of frames) {
+      const img = await openai.images.generate({
+        model: "gpt-image-1",
+        prompt: `black and white storyboard, cinematic, pencil-drawn: ${frame.description}`,
+        size: "1024x1024"
+      });
+
+      images.push({ url: img.data[0].url });
+    }
+
+    res.json({ images });
+  } catch (err) {
+    res.status(500).send("Error generating storyboards");
+  }
+});
+
+export default router;
