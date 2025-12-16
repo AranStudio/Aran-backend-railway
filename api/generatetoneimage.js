8716d5f5fc2604c05a7e774c200ae4de8987d823
AranStudio <Rileyscottneal@gmail.com>
2025-12-15 22:07:50 -0800

    Delete api/generatetoneimage.js

--- api/generatetoneimage.js
+++
@@ -1,7 +0,0 @@
-// generateToneImage.js
-import generate from "./generate.js";
-
-// Alias route: reuse the exact same behavior as /api/generate
-export default async function generateToneImage(req, res) {
-  return generate(req, res);
-}
